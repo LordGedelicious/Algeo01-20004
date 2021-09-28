4df212d75a3588801ea47672e088fce26ff6207f
arikrayi <83354451+arikrayi@users.noreply.github.com>
2021-09-28 11:33:10 +0700

    Add SPL Gauss
    buat ngedisplay tiap x nya belom beres baru bikin matriks echelon doang

--- src/menuSPL.java
+++ src/menuSPL.java
@@ -1,38 +1,37 @@
-import src.matriks;
+package src;
+
+import java.lang.reflect.Array;
 
 public class menuSPL {
-    public static void gaussMethod(double Matrix[][]) {
-    }
-    public static int rowEchelonElim(double Matrix[][], int dimension) {
-        for (int i = 0; i < dimension; i++) {
-            int i_max = i;
-            int v_max = (int)Matrix[i_max][i];
-
-            for (int j = i + 1; i < dimension; j++) {
-                if (Math.abs(Matrix[j][i]) > v_max) {
-                    v_max = (int)Matrix[j][i];
-                    i_max = j;
-                } 
-            }
-
-            if (Matrix[i][i_max] == 0) {
-                return i;
-            }
-
-            if (i_max != i) {
-                swap_row(Matrix, i, i_max);
-            }
-
-            for (int j = i + 1; j < dimension; j++) {
-                double f = Matrix[j][i] / Matrix[i][i];
-
-                for (int k = i + 1; k <= dimension; k++) {
-                    Matrix[j][k] -= Matrix[i][k] * f;
+    public static void SPLGauss(matriks m) {
+        for(int j = 0; j < m.cols-1; j++) {
+            int yankee = 0;   
+            while (m.isIdxValid(j, j+yankee) && m.elmt[j][j+yankee] != 1) {
+                if (m.elmt[j][j+yankee] != 1 && m.elmt[j][j+yankee] != 0) {
+                    double withno = m.elmt[j][j+yankee];
+                    for (int x=0; x<m.cols; x++) {
+                        m.elmt[j][x] /= withno;
+                    }
+                    break;
                 }
-
-                Matrix[j][i] = 0;
+                else if (m.elmt[j][j] == 0) {
+                    yankee ++;
+                }
+            }
+            for(int i = 1+j; i < m.rows; i++) {
+                if (m.elmt[i][j+yankee] != 0.0) {
+                    double brim = m.elmt[i][j+yankee];
+                    for (int k = 0; k < m.cols; k++) {
+                        m.elmt[i][k] = m.elmt[i][k] - brim/(m.elmt[j][j+yankee])*m.elmt[j][k];
+                    }
+                }
             }
         }
-        return 1;
+        // double[] ans = new double[m.cols-1];
+        // if ((m.rows >= m.cols) && (m.elmt[m.cols-2][m.cols-3] == 0) && (m.elmt[m.cols-2][m.cols-2] != 0)) {
+        //     ans[m.cols-2] = m.elmt[m.cols-2][m.cols-1]/m.elmt[m.cols-2][m.cols-2];
+        // }
+        // System.out.println("\nX" + ans.length + " adalah " + ans[m.cols-2]);
+        // buat ngedisplay tiap x nya belom beres baru bikin matriks echelon doang
     }
 }
