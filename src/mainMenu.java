80513694e279fdea55da062bb2a1cf996edf1868
LordGedelicious <gede.coolguy@gmail.com>
2021-09-30 14:56:11 +0700

    Update mainMenu.java
    fix untuk determinan, invers, spl (spl msh blm confirm buat yg metode invers)

--- src/mainMenu.java
+++ src/mainMenu.java
@@ -41,40 +41,75 @@ public class mainMenu {
             }
         }
     }
-    public static matriks readOrType() {
-        Scanner choice_input = new Scanner(System.in);
-        System.out.println("Pilih cara untuk mengisi matriks untuk diproses");
-        System.out.println("1. Input keyboard");
-        System.out.println("2. Baca dari file");
-        int choice = choice_input.nextInt();
-        choice_input.close();
-        switch (choice) {
-            case 1:
-                matriks input_matriks = initiateMatriks();
-                return input_matriks;
-            case 2:
-                // ini kalo read dari file
-                matriks input_matriks_1 = initiateMatriks();
-                return input_matriks_1; // ini biar kodenya jalan aja, nanti diganti
-            default:
-                // ini kalo selain dari 1 atau 2 inputnya
-                System.out.println("Input tidak dikenali, silahkan coba ulang");
-                return readOrType();
-        }
-    }
-    static matriks initiateMatriks() {
-        Scanner matriks_input = new Scanner(System.in);
-        // ukuran matriks adalah row_matriks x col_matriks
-        int row_matriks = matriks_input.nextInt();
-        int col_matriks = matriks_input.nextInt();
-        matriks spl = new matriks(row_matriks, col_matriks);
-        for (int i = 0; i < row_matriks; i++) {
-            for (int j = 0; j < col_matriks; j++) {
-                spl.elmt[i][j] = matriks_input.nextDouble();
+    static void Determinan() {
+        try (Scanner det_input = new Scanner(System.in)) {
+            System.out.println("Pilih Metode Untuk Mencari Determinan : ");
+            System.out.println("1. Metode Kofaktor");
+            System.out.println("2. Metode Reduksi Baris");
+            System.out.println("3. Kembali ke Menu Utama");
+            int det_int = det_input.nextInt();
+            switch (det_int) {
+                case 1:
+                    matriks solve_kofaktor = readOrType();
+                    double det_kofaktor = menuDeterminant.determinantKofaktor(solve_kofaktor);
+                    System.out.println("Determinan matriksnya adalah " + det_kofaktor);
+                    break;
+                case 2:
+                    matriks solve_reduksi = readOrType();
+                    double det_reduksi = menuDeterminant.determinantReduksiBaris(solve_reduksi);
+                    System.out.println("Determinan matriksnya adalah " + det_reduksi);
+                    break;
+                case 3:
+                    Menu();
+                    break;
+                default:
+                    Determinan();
+                    break;
             }
         }
-        matriks_input.close();
-        return spl;
+    }
+    static void Invers() {
+        matriks solve_invers = readOrType();
+        matriks solved_invers = menuInvers.inverse(solve_invers);
+        solved_invers.displayMatriks();
+    }
+    public static matriks readOrType() {
+        try (Scanner choice_input = new Scanner(System.in)) {
+            System.out.println("Pilih cara untuk mengisi matriks untuk diproses");
+            System.out.println("1. Input keyboard");
+            System.out.println("2. Baca dari file");
+            int choice = choice_input.nextInt();
+            switch (choice) {
+                case 1:
+                    matriks input_matriks = initiateMatriks();
+                    return input_matriks;
+                case 2:
+                    // ini kalo read dari file
+                    matriks input_matriks_1 = initiateMatriks();
+                    return input_matriks_1; // ini biar kodenya jalan aja, nanti diganti
+                default:
+                    // ini kalo selain dari 1 atau 2 inputnya
+                    System.out.println("Input tidak dikenali, silahkan coba ulang");
+                    return readOrType();
+            }
+        }
+    }
+    public static matriks initiateMatriks() {
+        try (Scanner matriks_input = new Scanner(System.in)) {
+            System.out.println("Silahkan input jumlah baris dalam matriksnya: ");
+            // ukuran matriks adalah row_matriks x col_matriks
+            int row_matriks = matriks_input.nextInt();
+            System.out.println("Silahkan input jumlah kolom dalam matriksnya: ");
+            int col_matriks = matriks_input.nextInt();
+            matriks spl = new matriks(row_matriks, col_matriks);
+            System.out.println("Silahkan input elemen dalam matriksnya: ");
+            for (int i = 0; i < row_matriks; i++) {
+                for (int j = 0; j < col_matriks; j++) {
+                    spl.elmt[i][j] = matriks_input.nextDouble();
+                }
+            }
+            return spl;
+        }
     }
     static void Menu() {
         try (Scanner first_input = new Scanner(System.in)) {
@@ -91,8 +126,10 @@ public class mainMenu {
                     SPL();
                     break;
                 case 2:
+                    Determinan();
                     break;
                 case 3:
+                    Invers();
                     break;
                 case 4:
                     break;
